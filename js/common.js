e58689574c09a359b5d211fcaac35cbb0fc40c5c
mdsajib50 <mdajib850@gmail.com>
2022-08-23 14:34:00 +0600

    common function add

---
+++ js/common.js
@@ -0,0 +1,30 @@
+let playerList;
+function player(elementId, playerId, playerListId){
+    const playerName = document.getElementById(playerId);
+
+        const playerValue = playerName.innerText;
+    
+        playerList = document.getElementById(playerListId);
+    
+        if(playerList.children.length < 5){
+
+        let li = document.createElement('li');
+    
+        li.setAttribute('id', 'list-item');
+
+        li.appendChild(document.createTextNode(playerValue));
+
+        playerList.appendChild(li);
+
+         document.getElementById(elementId).disabled = true;
+
+         document.getElementById(elementId).setAttribute('class', 'bg-gray-500 text-xl text-white py-2 px-8 text-center mx-20 mt-6');
+        
+        }else{
+            alert('Not more player add')
+        }
+}
+
+function validator(params) {
+    
+}
---
+++ js/script.js
@@ -0,0 +1,67 @@
+const selectBtn = document.getElementById('select-btn');
+
+
+selectBtn.addEventListener('click', function() {
+    player('select-btn', 'player-name', 'player-list');
+
+   
+})
+
+const selectBtn2 = document.getElementById('select-btn2');
+
+
+selectBtn2.addEventListener('click', function() {
+    player('select-btn2', 'player-name2', 'player-list');
+
+})
+
+const selectBtn3 = document.getElementById('select-btn3');
+
+
+selectBtn3.addEventListener('click', function() {
+    player('select-btn3', 'player-name3', 'player-list');
+
+})
+
+const selectBtn4 = document.getElementById('select-btn4');
+
+selectBtn4.addEventListener('click', function() {
+    player('select-btn4', 'player-name4', 'player-list');
+
+})
+
+const selectBtn5 = document.getElementById('select-btn5');
+
+selectBtn5.addEventListener('click', function() {
+    player('select-btn5', 'player-name5', 'player-list');
+
+})
+
+const selectBtn6 = document.getElementById('select-btn6');
+
+selectBtn6.addEventListener('click', function() {
+    player('select-btn6', 'player-name6', 'player-list');
+
+})
+
+const selectBtn7 = document.getElementById('select-btn7');
+
+selectBtn7.addEventListener('click', function() {
+    player('select-btn7', 'player-name7', 'player-list');
+
+})
+
+const selectBtn8 = document.getElementById('select-btn8');
+
+selectBtn8.addEventListener('click', function() {
+    player('select-btn8', 'player-name8', 'player-list');
+
+})
+
+const selectBtn9 = document.getElementById('select-btn9');
+
+selectBtn9.addEventListener('click', function() {
+    player('select-btn9', 'player-name9', 'player-list');
+})
+
+
